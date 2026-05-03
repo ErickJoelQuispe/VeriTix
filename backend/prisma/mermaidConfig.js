35f79e39f2f7c664eb99557394a53bff0ddb8a5a
Erick <erickjo10x@gmail.com>
2026-05-03 11:27:39 +0200

    feat: add ERD generators and mermaid config to prisma schema

---
+++ backend/prisma/mermaidConfig.js
@@ -0,0 +1,10 @@
+export const er = {
+  diagramPadding: 0,
+  useMaxWidth: false,
+  layoutDirection: 'TB',
+  nodeSpacing: 60,
+  rankSpacing: 90,
+  entityPadding: 12,
+  minEntityHeight: 72,
+  fontSize: 18,
+}
